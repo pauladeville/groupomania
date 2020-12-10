d928ee01180e9b2bcec7894609023a438bccd4af
Paula <paula.deville@posteo.net>
2020-12-10 14:23:09 +0100

    db connection

--- back/dbConnect.js
+++ back/dbConnect.js
@@ -1,12 +1,22 @@
 const mysql = require('mysql');
-const dotenv = require("dotenv").config();
+const dotenv = require("dotenv").config({path: ".env"});
 
 // Connexion à la base de données
-exports.connection = mysql.createPool({
-    host     : 'localhost',
-    user     : `${process.env.DB_USER}`,
-    password : `${process.env.DB_PASSWORD}`,
-    database : 'Groupomania',
-    timezone : 'local',
-    charset : 'utf8mb4'
-  });
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: `${process.env.DB_USER}`,
+  password: `${process.env.DB_PASSWORD}`,
+  database: 'Groupomania',
+  timezone: 'local',
+  charset: 'utf8mb4'
+});
+
+db.connect((error) => {
+  if(error) {
+    throw error;
+  } else {
+    console.log("Connecté à MySQL avec l\'ID " + db.threadId);
+  }
+});
+
+module.exports = db;
